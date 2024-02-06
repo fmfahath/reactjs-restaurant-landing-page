1fde0e382cd0836fa6665d54c45e56c916008e12
fmfahath <fmfahath.dev@gmail.com>
2024-02-06 20:24:24 +0530

    contact section : content & mobile view added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About';
+import Contact from './components/Contact';
 import Home from './components/Home';
 import Testimonial from './components/Testimonial';
 import Work from './components/Work';
@@ -11,6 +12,7 @@ function App() {
       <About />
       <Work />
       <Testimonial />
+      <Contact />
     </div>
   );
 }
