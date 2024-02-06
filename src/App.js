d0fb7cff252b9a18643f847cc6426841c82e36cf
fmfahath <fmfahath.dev@gmail.com>
2024-02-06 20:17:27 +0530

    work section : content & mobile view added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import About from './components/About';
 import Home from './components/Home';
+import Work from './components/Work';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Work />
     </div>
   );
 }
