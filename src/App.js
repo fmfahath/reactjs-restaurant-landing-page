95a11b5c59813399192f4e413dc2bf10852fc5db
fmfahath <fmfahath.dev@gmail.com>
2024-02-06 20:26:36 +0530

    footer section : content & mobile view added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import Testimonial from './components/Testimonial';
 import Work from './components/Work';
@@ -13,6 +14,7 @@ function App() {
       <Work />
       <Testimonial />
       <Contact />
+      <Footer />
     </div>
   );
 }
