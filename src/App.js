9d1d4c32b0080761669d62aba54f8efcb36d058b
fmfahath <fmfahath.dev@gmail.com>
2024-02-06 20:22:55 +0530

    testimonial section : content & mobile view added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './components/About';
 import Home from './components/Home';
+import Testimonial from './components/Testimonial';
 import Work from './components/Work';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <About />
       <Work />
+      <Testimonial />
     </div>
   );
 }
